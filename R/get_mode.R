26edf2ab04f6a9b8c2de58cf55fc263b9ef4bc2f
Yezhou <liyezhou123@gmail.com>
2024-12-17 00:30:56 +0000

    remove get_mode as already in rstatix

--- R/get_mode.R
+++ R/get_mode.R
@@ -1,18 +1,18 @@
-#' Get the Mode of a Vector
-#'
-#' This function calculates the mode of a vector, i.e., the most frequent value.
-#'
-#' @param x A vector of numeric or character values.
-#'
-#' @return The mode of the vector `x`, which is the value that appears most frequently.
-#'
-#' @examples
-#' get_mode(c(1, 2, 2, 3, 3, 3))
-#' get_mode(c("apple", "banana", "apple", "apple", "cherry"))
-#'
-#' @export
-
-get_mode <- function(x) {
-  ux <- unique(x)
-  ux[which.max(tabulate(match(x, ux)))]
-}
+# #' Get the Mode of a Vector
+# #'
+# #' This function calculates the mode of a vector, i.e., the most frequent value.
+# #'
+# #' @param x A vector of numeric or character values.
+# #'
+# #' @return The mode of the vector `x`, which is the value that appears most frequently.
+# #'
+# #' @examples
+# #' get_mode(c(1, 2, 2, 3, 3, 3))
+# #' get_mode(c("apple", "banana", "apple", "apple", "cherry"))
+# #'
+# #' @export
+#
+# get_mode <- function(x) {
+#   ux <- unique(x)
+#   ux[which.max(tabulate(match(x, ux)))]
+# }
